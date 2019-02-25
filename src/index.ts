cbb453c6a6b4c16586fec1d38a0bb125a147eaf3
gregnoble <greg@gregnoble.net>
2019-02-24 20:02:39 -0500

    remove jquery

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,49 @@
-import * as $ from "jquery";
 import './styles.css';
-import { roundToTwoPlaces } from './utils';
+import { addClass, removeClass, roundToTwoPlaces } from './utils';
 
-const tipAmount = $('#tipAmount');
-const totalAmount = $('#totalAmount');
-const checkAmount = document.getElementById('checkAmount') as HTMLInputElement; // this is how we did it in class
-const percent15btn = $("#15percent");
-const percent20btn = $("#20percent");
+const tipAmount = document.querySelector('#tipAmount') as HTMLInputElement; 
+const totalAmount = document.querySelector('#totalAmount') as HTMLInputElement;
+const checkAmount = document.getElementById('checkAmount') as HTMLInputElement;
+const percent15btn = document.getElementById('15percent');
+const percent20btn = document.getElementById('20percent');
+const percent25btn = document.getElementById('25percent');
 
-$('.percentRadio').on('click', (e)=>{
-    $('.percentRadio').prop('checked', false).parent().removeClass('active');
-    $(e.currentTarget).prop('checked', true).parent().addClass('active');
+function setActiveRadio(event:any){
+    document.querySelectorAll('.percentRadio').forEach((e:any)=>{
+        removeClass(e.parentElement, 'active');
+    });
+    if (event.currentTarget.value == 'on'){
+        addClass(event.currentTarget.parentElement, 'active');
+    }
     calculateStuff();
+}
+
+document.querySelectorAll('.percentRadio').forEach((e:any)=>{
+    e.addEventListener('click', setActiveRadio);
 });
 
+var matches = function(el:any, selector:any) {
+    return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
+  };
+
 let calculateStuff = function(){
-    if(!$.isNumeric(checkAmount.value)){
-        $('#checkAmount').addClass('error');
-        tipAmount.val(0);
-        totalAmount.val(0);
+    if(isNaN(parseFloat(checkAmount.value))){
+        addClass(checkAmount,'error');
+        tipAmount.setAttribute('value', '0');
+        totalAmount.setAttribute('value', '0');
     } else {
         let check:number = parseFloat(checkAmount.value);
-        $('#checkAmount').removeClass('error');
-        if (percent15btn.is(":checked")){
-            tipAmount.val(roundToTwoPlaces(check * .15));
-            totalAmount.val(roundToTwoPlaces(check * 1.15));
-        } else if (percent20btn.is(":checked")) {
-            tipAmount.val(roundToTwoPlaces(check * .20));
-            totalAmount.val(roundToTwoPlaces(check * 1.20));
+        removeClass(checkAmount,'error');
+                 
+        if (matches(percent15btn,":checked")){
+            tipAmount.setAttribute('value', roundToTwoPlaces(check * .15));
+            totalAmount.setAttribute('value', roundToTwoPlaces(check * 1.15));
+        } else if (matches(percent20btn,":checked")) {
+            tipAmount.setAttribute('value', roundToTwoPlaces(check * .20));
+            totalAmount.setAttribute('value', roundToTwoPlaces(check * 1.20));
         } else {
-            tipAmount.val(roundToTwoPlaces(check * .25));
-            totalAmount.val(roundToTwoPlaces(check * 1.25));
+            tipAmount.setAttribute('value', roundToTwoPlaces(check * .25));
+            totalAmount.setAttribute('value', roundToTwoPlaces(check * 1.25));
         }
     }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,21 @@
 export function roundToTwoPlaces(amount: number) {
     return amount.toFixed(2);
 }
+
+export function matches(el:any, selector:any) {
+    return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
+};
+
+export function addClass(el:any,className:string){
+    if (el.classList)
+        el.classList.add(className);
+    else
+        el.className += ' ' + className;
+}
+
+export function removeClass(el:any, className:string){
+    if (el.classList)
+        el.classList.remove(className);
+    else
+        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
